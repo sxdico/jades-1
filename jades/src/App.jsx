580aba00dd78f36273e7fe7b5b06942b6d4ee108
CarlosErz <rosariosalda25@gmail.com>
2023-04-21 14:10:03 -0500

    jades

--- jades/src/App.jsx
+++ jades/src/App.jsx
@@ -1,16 +1,16 @@
 
 import '../src/css/style.css'
-import {Hero} from './components/Hero.jsx'
+import { Hero } from './components/Hero.jsx'
 import { QuienesSomos } from './components/QuienesSomos'
 function App() {
 
   return (
-  <div className="layaout">
-    <Hero></Hero>
-    <QuienesSomos></QuienesSomos>
-    
-  </div>
-   
+    <div className="layaout">
+      <Hero></Hero>
+      <QuienesSomos></QuienesSomos>
+
+    </div>
+
   )
 }
 
