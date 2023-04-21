9bb8e91c18ba214b479dc37e7edac3474a63b32c
CarlosErz <rosariosalda25@gmail.com>
2023-04-21 15:06:28 -0500

    btn navbar

--- jades/src/App.jsx
+++ jades/src/App.jsx
@@ -2,13 +2,14 @@
 import '../src/css/style.css'
 import { Hero } from './components/Hero.jsx'
 import { QuienesSomos } from './components/QuienesSomos'
+import { Navbar } from './components/Navbar.jsx'
 function App() {
 
   return (
     <div className="layaout">
+      <Navbar></Navbar>
       <Hero></Hero>
       <QuienesSomos></QuienesSomos>
-
     </div>
 
   )
---
+++ jades/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+export function Navbar() {
+  return(
+  <div className="Navbar">
+    <div className="container">
+      <div className="title">
+        <h1 className="logo">JADES</h1>
+      </div>
+      <div className="content">
+        <nav>
+
+        </nav>
+      </div>
+      <button className="btn_navbar">
+        <span className="container_btn">
+         <span className="top">
+         </span>
+         <span className="middle"></span>
+        </span>
+      </button>
+
+
+    </div>
+  </div>
+  )
+}
