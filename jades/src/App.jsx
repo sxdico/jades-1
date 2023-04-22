e51fd16e20045e1a2e0336ee993d271ff8d08b3e
CarlosErz <rosariosalda25@gmail.com>
2023-04-22 10:36:17 -0500

    Loader  y paquetes

--- jades/src/App.jsx
+++ jades/src/App.jsx
@@ -1,16 +1,39 @@
-
 import '../src/css/style.css'
 import { Hero } from './components/Hero.jsx'
 import { QuienesSomos } from './components/QuienesSomos'
 import { Navbar } from './components/Navbar.jsx'
+import { useState, useEffect } from 'react';
+import { Carga } from './components/Carga.jsx';
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="layaout">
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <QuienesSomos></QuienesSomos>
+    <div>
+      {isLoading ? (
+        <Carga></Carga>
+      ) : (
+        <div className="layaout">
+          <header>
+            <Navbar></Navbar>
+          </header>
+          <Hero></Hero>
+          <section>
+            <QuienesSomos></QuienesSomos>
+          </section>
+        </div>
+
+      )}
     </div>
+
 
   )
 }
---
+++ jades/src/components/Carga.jsx
@@ -0,0 +1,12 @@
+import '../css/var.css'
+export function Carga() {
+  return (
+    <section className='base'>
+      <div className="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  )
+}
